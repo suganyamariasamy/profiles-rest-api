4fee2e72063563ef60d1a5b9a3d8c33bc15c75bf
Suganya <suganya@neosme.com>
2024-10-07 21:18:07 +0530

    edited license

--- profile_api/admin.py
+++ profile_api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from profile_api import models
+admin.site.register(models.UserProfile)
--- profile_api/models.py
+++ profile_api/models.py
@@ -15,15 +15,14 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name = name)
         user.set_password(password)
         user.save(using=self._db)
-        return User
+        return user
 
 
-        def create_superuser(self, email, name, password):
-            user = self.create_user(email, name, password)
-            user.is_superuser = True
-            user.is_staff = True
-            user.save(using=self._db)
-
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
